2131e4fc81e50f47ac56494f7bd23e6d71866e8b
h2qisme <874175299@qq.com>
2023-08-10 11:08:51 +0800

    添加功能：全选，删除整体等

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
  * @Author: h2qisme 874175299@qq.com
  * @Date: 2023-08-07 10:51:40
  * @LastEditors: h2qisme 874175299@qq.com
- * @LastEditTime: 2023-08-09 16:00:34
+ * @LastEditTime: 2023-08-10 11:00:41
  * @FilePath: \vue_test\src\App.vue
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 -->
@@ -12,7 +12,7 @@
 			<div class="todo-wrap">
 			<MyHeader :addTodo="addTodo"></MyHeader>
 			<MyList :todos="todos" :checkTodo="checkTodo" :deleteTodo="deleteTodo"></MyList>
-			<MyFooter></MyFooter>
+			<MyFooter :todos="todos" :checkAllTodo="checkAllTodo" :clearAllTodo="clearAllTodo"></MyFooter>
 			</div>
 		</div>
 	</div>	
@@ -50,6 +50,16 @@
 			//删除一个todo
 			deleteTodo(id){
 				this.todos = this.todos.filter( todo => todo.id !== id )
+			},
+			// 全选or取消全选 
+			checkAllTodo(done){
+				this.todos.forEach((todo) =>{
+					todo.done = done
+				})
+			},
+			// clear all 清除所有已经完成的todo
+			clearAllTodo(){
+				this.todos = this.todos.filter( todo => !todo.done)
 			}
 		}
 	}
--- src/components/MyFooter.vue
+++ src/components/MyFooter.vue
@@ -1,18 +1,54 @@
+<!--
+ * @Author: h2qisme 874175299@qq.com
+ * @Date: 2023-08-07 10:51:28
+ * @LastEditors: h2qisme 874175299@qq.com
+ * @LastEditTime: 2023-08-10 11:05:51
+ * @FilePath: \vue_test\src\components\MyFooter.vue
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+-->
 <template>
-	<div class="todo-footer">
+	<div class="todo-footer" v-show="total">
 		<label>
-		<input type="checkbox"/>
+		<input type="checkbox" v-model="isAll"/>
 		</label>
 		<span>
-		<span>已完成0</span> / 全部2
+		<span>已完成{{doneTotal}}</span> / 全部{{todos.length}}
 		</span>
-		<button class="btn btn-danger">清除已完成任务</button>
+		<button class="btn btn-danger" @click="clearAll">清除已完成任务</button>
 	</div>
 </template>
 
 <script>
 	export default {
 		name: 'MyFooter',
+		props:['todos','checkAllTodo','clearAllTodo'],
+		computed: {
+			total(){
+				return this.todos.length
+			},
+			doneTotal(){
+				let i = 0
+				this.todos.forEach((todo) => {
+					if(todo.done)
+					i++
+				})
+				return i
+			},
+			isAll:{
+				get(){
+					return this.doneTotal === this.total && this.total > 0
+				},
+				set(value){
+					this.checkAllTodo(value)
+				}
+			},			
+		},
+		methods: {
+			clearAll(){
+				this.clearAllTodo()
+			}
+		}
+
 	}
 </script>
 
